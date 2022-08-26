7536b78e6c1106cfde2362b2ace5125ac9f8df91
Johannes Moreno <idevelop25@gmail.com>
2022-08-26 16:53:57 -0500

    Primer commit

---
+++ webpack.prod.js
@@ -0,0 +1,76 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+
+
+module.exports = {
+    mode: 'production',
+
+    output:{
+        clean: true,
+        filename: 'main.[fullhash].js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /styles.css$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            }
+        ],
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer:[
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    },
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            title: 'My WebPack App',
+            // filename: 'index.html',
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[fullhash].css',
+            ignoreOrder: false,
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets/', to: 'assets/'}
+            ]
+        }),
+    ],
+}
